beec46c70e7f0bee51e365cfb615bcf6d974c3b9
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-01-26 15:51:18 -0800

    working play button w jquery onclick

--- jukebox.js
+++ jukebox.js
@@ -3,12 +3,20 @@
 // Prompts the user to enter a song.
 // Plays the entered song.
 // Repeats when the song is complete.
-//
-var promptAndPlay = function() {
-  var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
-  var song = parseSong(songString);
-  playSong(song, 500, promptAndPlay);
-};
+// //
+// var promptAndPlay = function() {
+//   var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
+//   var song = parseSong(songString);
+//   playSong(song, 500, promptAndPlay);
+// };
 
-// Get things going.
-promptAndPlay();
+// // // Get things going.
+// promptAndPlay();
+
+$(document).ready(function() {
+  $('button').on('click', function(){
+    var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
+    var song = parseSong(songString);
+    playSong(song, 500);
+  });
+});
