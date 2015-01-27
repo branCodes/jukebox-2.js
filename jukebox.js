3560e5b767b4da963d1a6d2ba8147c564d0c2a64
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-01-26 16:10:38 -0800

    working jukebox.. play button needs modifying

--- jukebox.js
+++ jukebox.js
@@ -15,8 +15,12 @@
 
 $(document).ready(function() {
   $('button').on('click', function(){
+    $(this).html('playing.....');
+    $(this).attr('disabled', true)
     var songString = prompt("Welcome to JukeBox!\nEnter a song to play:");
     var song = parseSong(songString);
     playSong(song, 500);
+    $(this).html("Play!");
+    $(this).attr('disabled', false)
   });
 });
